1dc0c795c4237bdc12acd4b2e1e47ff1dac8d604
Yaov05 <ortizalejandro551199@gmail.com>
2023-07-27 12:43:50 -0500

    fix: ajuste archivo de migraciones tabla aprobacion

--- database/migrations/20230727_120337_agregar_tabla_aprobacion.go
+++ database/migrations/20230727_120337_agregar_tabla_aprobacion.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/beego/beego/v2/client/orm/migration"
 )
 
@@ -20,11 +24,28 @@ func init() {
 // Run the migrations
 func (m *AgregarTablaAprobacion_20230727_120337) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-
+	file, err := ioutil.ReadFile("../scripts/20230727_120337_agregar_tabla_aprobacion_up.sql")
+	if err != nil {
+		fmt.Println(err)
+	}
+	requests := strings.Split(string(file), ";")
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
 }
 
 // Reverse the migrations
 func (m *AgregarTablaAprobacion_20230727_120337) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
+	file, err := ioutil.ReadFile("../scripts/20230727_120337_agregar_tabla_aprobacion_down.sql")
 
+	if err != nil {
+		fmt.Println(err)
+	}
+	requests := strings.Split(string(file), ";")
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
 }
